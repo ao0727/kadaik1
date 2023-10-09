367b2cb16752a1e99af1baaa0ed6a417074023a3
ao0727 <qingduj@gmail.com>
2023-10-09 23:53:29 +0900

    プロジェクト ファイルを追加します。

---
+++ kadaik1/kadaik1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+//Q.2
+int sum(int a, int b);
+
+//Q3
+static int mult(int c, int d) 
+{
+    return c * d;
+}
+
+int main() {
+    
+    //Q.2
+    /*int num1 = 5;
+    int num2 = 3;
+    int result = sum(num1, num2);
+
+    cout << "Sum: " << result << endl;
+    */
+    
+    //Q.3
+    int num1 = 5;
+    int num2 = 3;
+    int result = mult(num1, num2);
+
+    cout << "Product: " << result << endl;
+
+
+
+
+
+}
